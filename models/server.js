140c7f92b75705d0bf7f6c58d443b3de02065c67
Edder Silva <arskang@gmail.com>
2020-12-27 19:29:55 -0600

    Mejoras

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 const Sockets = require("./sockets");
+const cors = require("cors");
 
 const Servidor = function (port) {
 
@@ -20,6 +21,8 @@ const Servidor = function (port) {
 Servidor.prototype.middlewares = function () {
     // Desplegar el directorio público
     this.app.use(express.static(path.join(__dirname, "../public")));
+    // CORS
+    this.app.use(cors());
 };
 
 Servidor.prototype.configurarSockets = function () {
--- models/sockets.js
+++ models/sockets.js
@@ -1,28 +1,61 @@
 
 const Sockets = function(io) {
     this.io = io;
-    this.mensajes = [{ mensaje: "Bienvenido a todos", usuario: "Administrador", fecha: new Date() }];
+    this.usuarios = [{ id: 1, usuario: "Administrador" }];
+    this.mensajes = [{
+        mensaje: "Bienvenido a todos",
+        usuario: this.usuarios[0],
+        fecha: new Date(),
+        tipo: "mensaje"
+    }];
     this.socketsEvents();
 };
+
+Sockets.prototype.usuarioExiste = function(info) {
+    return this.usuarios.filter(usu => (usu.usuario === info || usu.id === info));
+}
+
+Sockets.prototype.nuevosUsuarios = function(id) {
+    return this.usuarios.filter(usu => usu.id !== id);
+}
+
+Sockets.prototype.customMensaje = function(tipo, usuario) {
+    return {
+        tipo: tipo,
+        mensaje: tipo === "login" ? `${usuario} se ha unido al chat` : `${usuario[0].usuario} ha salido del chat`
+    }
+}
 
 Sockets.prototype.socketsEvents = function() {
     this.io.on("connection", (socket) => {
 
-        // socket.emit('mensaje-bienvenida', {
-        //     msg: "Bienvenido al servidor",
-        //     fecha: new Date()
-        // });
-
-        // socket.on('nuevo-cliente', (mensaje) => {
-        //     console.log("Cliente conectado", mensaje, socket.id);
-        // });
-
         this.io.emit('mensajes-chat', this.mensajes);
 
-        socket.on("mensaje-cliente", (mensaje) => {
-            this.mensajes = [...this.mensajes, mensaje];
+        socket.on("nuevo-usuario", (usuario) => {
+            let existe = this.usuarioExiste(usuario);
+            if(Array.isArray(existe) && existe.length > 0) {
+                socket.emit('error', "Actualmente ya existe un usuario con ese nombre en la sala");
+                return;
+            }
+            this.usuarios = [...this.usuarios, { usuario, id: socket.id}];
+            this.mensajes = [...this.mensajes, this.customMensaje("login", usuario)];
+            socket.emit('activar-chat');
             this.io.emit('mensajes-chat', this.mensajes);
-        })
+        });
+
+        socket.on("disconnect", () => {
+            let newUsuarios = this.nuevosUsuarios(socket.id);
+            if(newUsuarios.length === this.usuarios.length) return;
+            let usuario = this.usuarioExiste(socket.id);
+            this.usuarios = [...newUsuarios];
+            this.mensajes = [...this.mensajes, this.customMensaje("logoff", usuario)];
+            this.io.emit('mensajes-chat', this.mensajes);
+        });
+
+        socket.on("mensaje-cliente", ({ usuario, ...rest }) => {
+            this.mensajes = [...this.mensajes, { usuario: { usuario, id: socket.id }, ...rest }];
+            this.io.emit('mensajes-chat', this.mensajes);
+        });
 
     });
 };
